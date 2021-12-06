2a31f6e108ad143da28002e73cb20da3c3178c23
NiklasBara <n.baranowski@mail.de>
2021-12-06 18:37:00 +0100

    part1

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day6/Day6.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day6/Day6.scala
@@ -4,13 +4,38 @@ import java.nio.charset.StandardCharsets
 import scala.io.Source
 
 object Day6 extends App {
-  val INPUT_PATH = Inputs.test
+  val INPUT_PATH = Inputs.local
   val source = Source.fromFile(INPUT_PATH.toString, StandardCharsets.UTF_8.toString)
   val lines = source.getLines().toSeq
   source.close()
-  val inputs = lines.head
+  val inputs = lines.head.split(",").map(_.toInt)
 
-  println(inputs)
+  private val analyzer: FishAnalyzer = FishAnalyzer(inputs)
+
+  val res = analyzer.inDays(80)
+  println(s"part1: ${res.fish.size}")
+}
+
+case class FishAnalyzer(fish: Seq[Int]) {
+
+  def inDays(days: Int, analyzer: FishAnalyzer = this): FishAnalyzer = {
+    if(days <= 0){
+      return analyzer
+    }
+    inDays(days - 1, analyzer.nextDay())
+  }
+
+  def nextDay() = {
+    this.copy(
+      fish = fish.groupMapReduce(identity)(_ => 1)(_ + _).flatMap(entry => {
+        if (entry._1 == 0) {
+          Seq.fill(entry._2)(6).concat(Seq.fill(entry._2)(8))
+        } else {
+          Seq.fill(entry._2)(entry._1 - 1)
+        }
+      }).toSeq
+    )
+  }
 }
 
 object Inputs extends Enumeration {
