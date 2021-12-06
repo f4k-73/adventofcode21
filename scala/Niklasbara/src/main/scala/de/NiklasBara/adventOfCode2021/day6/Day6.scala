c3116eb4972ea3085ea121bf02620369cc5200a4
NiklasBara <n.baranowski@mail.de>
2021-12-06 19:49:55 +0100

    part 2 in under 1 sec

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day6/Day6.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day6/Day6.scala
@@ -1,6 +1,7 @@
 package de.NiklasBara.adventOfCode2021.day6
 
 import java.nio.charset.StandardCharsets
+import java.time.LocalDate
 import scala.io.Source
 
 object Day6 extends App {
@@ -8,15 +9,19 @@ object Day6 extends App {
   val source = Source.fromFile(INPUT_PATH.toString, StandardCharsets.UTF_8.toString)
   val lines = source.getLines().toSeq
   source.close()
-  val inputs = lines.head.split(",").map(_.toInt).groupMapReduce(identity)(_ => 1)(_ + _)
+  val inputs = lines.head.split(",").map(_.toInt).groupMapReduce(identity)(_ => BigInt(1))(_ + _)
 
   private val analyzer: FishAnalyzer = FishAnalyzer(inputs)
 
-  val res = analyzer.inDays(256)
-  println(s"part1: ${res.fish.values.sum}")
+  println(s"part1 ${analyzer.inDays(80).fish.values.sum}")
+
+  val t1 = System.nanoTime
+  val res2 = analyzer.inDays(256)
+  val duration = (System.nanoTime - t1) / 1e9d
+  println(s"part2 ${res2.fish.values.sum} in ${duration}s")
 }
 
-case class FishAnalyzer(fish: Map[Int, Int]) {
+case class FishAnalyzer(fish: Map[Int, BigInt]) {
 
   def inDays(days: Int, analyzer: FishAnalyzer = this): FishAnalyzer = {
     if(days <= 0){
