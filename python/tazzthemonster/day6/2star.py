bfc034f561f18a9fdd8283b6f93ea9a3c4c9bb27
Felix Pape <Felix_Pape@ymail.com>
2021-12-07 12:48:17 +0100

    day6 ⭐⭐

---
+++ python/tazzthemonster/day6/2star.py
@@ -0,0 +1,32 @@
+class Fish:
+
+    def __init__(self, timer=8):
+        self.timer = timer
+
+    def __repr__(self):
+        return str(self.timer)
+
+    def spawn(self):
+        if self.timer <= 0:
+            self.timer = 6
+            return Fish()
+        else:
+            self.timer -= 1
+
+
+file = open('data/day6.txt', 'r')
+fishes = []
+for line in file.readlines():
+    allValues = line.rstrip("\n").split(",")
+    for f in allValues:
+        fishes.append(Fish(int(f)))
+
+for i in range(256):
+    spawnedFishes = []
+    for f in fishes:
+        newFish = f.spawn()
+        if newFish:
+            spawnedFishes.append(newFish)
+    fishes += spawnedFishes
+
+print(len(fishes))
