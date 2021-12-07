109202a3fee85c24ceb732836844e7af245fd18f
NiklasBara <n.baranowski@mail.de>
2021-12-07 18:07:17 +0100

    refactor to use callback as fuelcalculator

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day7/Day7.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day7/Day7.scala
@@ -1,5 +1,7 @@
 package de.NiklasBara.adventOfCode2021.day7
 
+import de.NiklasBara.adventOfCode2021.day7
+
 import java.nio.charset.StandardCharsets
 import scala.io.Source
 
@@ -12,45 +14,34 @@ object Day7 extends App {
 
   private val analyzer: CrabAnalyzer = CrabAnalyzer(inputs)
 
-  println(s"part1 ${analyzer.moveLeast(true)}")
-  println(s"part1 ${analyzer.moveLeast(false)}")
+  println(s"part1 ${analyzer.moveLeast(FuelCalculators.SameAsSpaces)}")
+  println(s"part1 ${analyzer.moveLeast(FuelCalculators.PlusOnePerSpace)}")
 }
 
 case class CrabAnalyzer(crabToPos: Map[Int, Int]) {
 
-  def moveLeast(staticFuelCosts: Boolean): Int = {
+  def moveLeast(calculateFuel: Int => Int): Int = {
     val positions = crabToPos.keys.min to crabToPos.keys.max
-    positions.map(fuelToMoveCrabsIntoPos(crabToPos, _, staticFuelCosts)).min
+    positions.map(fuelToMoveCrabsIntoPos(crabToPos, _, calculateFuel)).min
   }
 
-  private def fuelToMoveCrabsIntoPos(crabs: Map[Int, Int], position: Int, staticFuelCosts: Boolean): Int = {
-    crabs.map(
-      if (staticFuelCosts)
-        fuelToMoveCrabIntoPos(_, position)
-      else
-        fuelToMoveCrabIntoPosWithGrowingFuelCosts(_, position)
-    ).sum
+  private def fuelToMoveCrabsIntoPos(crabs: Map[Int, Int], position: Int, calculateFuel: Int => Int): Int = {
+    crabs.map(fuelToMoveCrabIntoPos(_, position, calculateFuel)).sum
   }
 
-  private def fuelToMoveCrabIntoPos(crab: (Int, Int), position: Int): Int = {
-    Math.abs(crab._1 - position) * crab._2
+  private def fuelToMoveCrabIntoPos(crab: (Int, Int), position: Int, calculateFuel: Int => Int): Int = {
+    calculateFuel(Math.abs(crab._1 - position)) * crab._2
   }
+}
 
-  private def fuelToMoveCrabIntoPosWithGrowingFuelCosts(crab: (Int, Int), position: Int): Int = {
-    fuelCosts(Math.abs(crab._1 - position)) * crab._2
-  }
-
-  private def fuelCosts(n: Int): Int = {
-    n * (n + 1) / 2
-  }
-
+object FuelCalculators extends Enumeration {
+  val SameAsSpaces: Int => Int = (a: Int) => a
+  val PlusOnePerSpace: Int => Int = (spacesToMove: Int) => spacesToMove * (spacesToMove + 1) / 2
 }
 
 object Inputs extends Enumeration {
   type Inputs = Value
-  val test = Value("src/main/resources/day7/test_input.txt")
-  val local = Value("src/main/resources/day7/input.txt")
-  val shared = Value("../../data/day7.txt")
+  val test: day7.Inputs.Value = Value("src/main/resources/day7/test_input.txt")
+  val local: day7.Inputs.Value = Value("src/main/resources/day7/input.txt")
+  val shared: day7.Inputs.Value = Value("../../data/day7.txt")
 }
-
-
