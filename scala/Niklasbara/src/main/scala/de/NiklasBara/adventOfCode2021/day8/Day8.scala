7b96d7998edd1c2dc89fb7960c3d3bf620da8712
NiklasBara <n.baranowski@mail.de>
2021-12-08 20:34:02 +0100

    day8 -> part2 using class extensions :D

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day8/Day8.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day8/Day8.scala
@@ -11,15 +11,57 @@ object Day8 extends App {
   source.close()
   val inputsToOutputs = lines.map(_.split(" \\| ")).map(arr => arr(0) -> arr(1)).toMap
 
-  val uniqueSegments = Seq(2, 3, 4, 7)
+  val uniques = Map((2, "1"), (4, "4"), (3, "7"), (7, "8"))
+
+  val fiveSegmentLookups = Map(
+    ((2, 3), "3"),
+    ((1, 2), "2"),
+    ((1, 3), "5"),
+  )
+  val sixSegmentLookups = Map(
+    ((2, 4), "9"),
+    ((2, 3), "0"),
+    ((1, 3), "6"),
+  )
+
+  val segmentLookupTable = Map(
+    (5, (overlappingOne: Int, overlappingFour: Int) => fiveSegmentLookups.getOrElse((overlappingOne, overlappingFour), "")),
+    (6, (overlappingOne: Int, overlappingFour: Int) => sixSegmentLookups.getOrElse((overlappingOne, overlappingFour), "")),
+  )
 
 
   val countOfUniques = inputsToOutputs.values.map(_.trim)
     .flatMap(_.split(" ")).map(_.length)
-    .count(uniqueSegments.contains(_))
-
+    .count(uniques.keys.toSet.contains(_))
   println("part1:", countOfUniques)
 
+  val decoded = inputsToOutputs.map(decodeSevenSegment).sum
+  println("part2:", decoded)
+
+
+
+
+  def decodeSevenSegment(keyValue: (String, String)): Int = {
+    val keyNumbers = keyValue._1.split(" ")
+    val valueNumbers = keyValue._2.split(" ")
+
+    val oneSegments = (keyNumbers ++ valueNumbers).find(s => s.length == 2).get
+    val fourSegments = (keyNumbers ++ valueNumbers).find(s => s.length == 4).get
+
+    valueNumbers.map(numberString => uniques.getOrRun(
+      numberString.length, () =>
+        segmentLookupTable(numberString.length)(
+          numberString.intersect(oneSegments).length,
+          numberString.intersect(fourSegments).length
+        ))).mkString.toInt
+  }
+
+  implicit class BetterMap[A, B](val m: Map[A, B]) {
+    def getOrRun(key: A, provider: () => B) = m.get(key) match {
+      case Some(value) => value
+      case None => provider()
+    }
+  }
 }
 
 
