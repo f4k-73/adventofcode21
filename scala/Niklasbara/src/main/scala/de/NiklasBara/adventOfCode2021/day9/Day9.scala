ce17c60d4f6617322a9a14b05f5b1b24a7bd25a1
NiklasBara <n.baranowski@mail.de>
2021-12-10 17:20:54 +0100

    refactor day9 - part1

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day9/Day9.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day9/Day9.scala
@@ -9,61 +9,38 @@ object Day9 extends App {
   val lines = source.getLines().toSeq
   source.close()
 
-  private val value: Seq[Seq[Int]] = lines.map(_.toCharArray.map(_.asDigit).toSeq)
-
-  var cells: Seq[Cell] = Seq()
-  for (y <- value.indices) {
-    for (x <- 0 until value.head.size) {
-      val top = value.getNeighbourOrElse(y, x, OffsetsYX.top, Int.MaxValue)
-      val left = value.getNeighbourOrElse(y, x, OffsetsYX.left, Int.MaxValue)
-      val bottom = value.getNeighbourOrElse(y, x, OffsetsYX.bottom, Int.MaxValue)
-      val right = value.getNeighbourOrElse(y, x, OffsetsYX.right, Int.MaxValue)
-
-      cells = cells.appended(Cell(value.get(y, x), top, left, right, bottom))
-    }
-  }
+  val value: Seq[Seq[Int]] = lines.map(_.toCharArray.map(_.asDigit).toSeq)
 
 
-  println("part1:", cells.filter(_.isLowPoint).map(_.value + 1).sum)
+  val offsets = Seq((-1, 0), (0, -1), (1, 0), (0, 1))
 
 
+  println("part1:", value.lowPoints().map(_._1 + 1).sum)
 
-  implicit class Better2dSeq[T](val s: Seq[Seq[T]]) {
-    def getNeighbourOrElse(y: Int, x: Int, offset: (Int, Int), other: T): T = {
-      val targetY = y + offset._1
-      val targetX = x + offset._2
 
-      if (targetX < 0 || targetY < 0) {
-        other
-      } else if (targetY > s.length - 1 || targetX > s(targetY).length - 1) {
-        other
+  implicit class Better2dSeq(val s: Seq[Seq[Int]]) {
+    def getCell(y: Int, x: Int): (Int, Int, Int) = {
+      if (x < 0 || y < 0) {
+        (Int.MaxValue, y, x)
+      } else if (y > s.length - 1 || x > s(y).length - 1) {
+        (Int.MaxValue, y, x)
       } else {
-        s(targetY)(targetX)
+        (s(y)(x), y, x)
       }
     }
 
+    def getNeighbour(y: Int, x: Int, offset: (Int, Int)) = getCell(y + offset._1, x + offset._2)
+
+    def isLowPoint(y: Int, x: Int) = offsets.map(getNeighbour(y, x, _)).map(_._1).forall(_ > getCell(y, x)._1)
+
+    def lowPoints() = s.zipWithIndex
+      .flatMap(lineAndIndex => lineAndIndex._1.zipWithIndex
+        .filter(cellAndIndex => isLowPoint(lineAndIndex._2, cellAndIndex._2))
+        .map(cellAndIndex => (cellAndIndex._1, lineAndIndex._2, cellAndIndex._2))
+      )
+
     def get(y: Int, x: Int) = s(y)(x)
   }
-}
-
-case class Cell(
-                 value: Int,
-                 neighbourTop: Int,
-                 neighbourLeft: Int,
-                 neighbourRight: Int,
-                 neighbourBottom: Int,
-               ) {
-
-  def isLowPoint = {
-    Seq(neighbourTop, neighbourLeft, neighbourBottom, neighbourRight).forall(_ > value)
-  }
-}
-
-object OffsetsYX extends Enumeration {
-  val top: (Int, Int) = (-1, 0)
-  val left: (Int, Int) = (0, -1)
-  val bottom: (Int, Int) = (1, 0)
-  val right: (Int, Int) = (0, 1)
 }
 
 object Inputs extends Enumeration {
