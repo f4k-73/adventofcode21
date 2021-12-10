7d4d2a9d22930427ca2bb28e01b6da5526e07dc4
Dean Eckert <dean.eckert@red-oak-consulting.com>
2021-12-10 23:09:01 +0100

    not so beautiful part 2 of day9 but ⭐️⭐️

--- scala/eeedean/src/main/scala/de/edean/aoc2021/day9/Day9.scala
+++ scala/eeedean/src/main/scala/de/edean/aoc2021/day9/Day9.scala
@@ -13,23 +13,53 @@ object Day9 extends App {
 
   val maxX = input.head.size - 1
   val maxY = input.size - 1
-  // println(s"$maxX x $maxY")
+  case class Coord(x: Int, y: Int)
+  implicit object CoordOrdering extends Ordering[Coord] {
+    override def compare(x: Coord, y: Coord): Int = {
+      val xOrder =x.x.compare(y.x)
+      if (xOrder == 0)
+        x.y.compare(y.y)
+      else xOrder
+    }
+  }
 
-  //val coords = input.map(_.zipWithIndex).zipWithIndex.flatMap({ case (xs, y) => xs.map(x => ((x._2, y), x._1.asDigit)) }).toMap
-  val coords = input.zipWithIndex.map({ case (str, i) => (i, str.zipWithIndex) }).flatMap({ case (y, xs) => xs.map({case (char, x) => ((x, y), char.asDigit)})}).toMap
+  val coords = input.zipWithIndex.map({ case (str, i) => (i, str.zipWithIndex) }).flatMap({ case (y, xs) => xs.map({case (char, x) => (Coord(x, y), char.asDigit)})}).toMap
   val sortedCoordSeq = coords.toSeq.sorted
-  // println((0 to maxY).map(y => (0 to maxX).map(x => coords(x, y)).mkString).mkString("\n"))
-  def neighbours(x: Int, y: Int): Seq[Int] = {
+  // println((0 to maxY).map(y => (0 to maxX).map(x => coords(Coord(x, y))).mkString).mkString("\n"))
+  def findNeighbourVals(c: Coord): Seq[Int] = {
+    val (left, right, top, bottom) = (Coord(c.x - 1, c.y), Coord(c.x + 1, c.y), Coord(c.x, c.y - 1), Coord(c.x, c.y + 1))
     Seq(
-      coords.get((x - 1, y)).getOrElse(10),
-      coords.get((x + 1, y)).getOrElse(10),
-      coords.get((x, y - 1)).getOrElse(10),
-      coords.get((x, y + 1)).getOrElse(10)
+      coords.get(left).getOrElse(10),
+      coords.get(right).getOrElse(10),
+      coords.get(top).getOrElse(10),
+      coords.get(bottom).getOrElse(10)
     )
   }
   // if all neighbours are lower / value is higher than/equal to none of them => it's a low point!
-  val lowPoints = coords.filter({ case ((x, y), value) => neighbours(x, y).filter(_ <= value).isEmpty })
+  val lowPoints = coords.filter({ case (coord, value) => findNeighbourVals(coord).filter(_ <= value).isEmpty })
   println(s"There are ${lowPoints.size} low points: $lowPoints")
   val riskLevelSum = lowPoints.map(_._2 + 1).sum
   println(s"Part 1: $riskLevelSum")
+
+  def exploreBasin(c: Coord, oldResultSet: Set[(Coord, Int)] = Set()): Set[(Coord, Int)] = {
+    val (left, right, top, bottom) = (Coord(c.x - 1, c.y), Coord(c.x + 1, c.y), Coord(c.x, c.y - 1), Coord(c.x, c.y + 1))
+    var result = oldResultSet + coords.get(c).map((c, _)).get
+    def enhanceResult(coord: Coord) = {
+      val opt = coords.get(coord).map((coord, _))
+      if (opt.exists(_._2 < 9) && !result.contains(opt.get))
+        result = result ++ exploreBasin(coord, result)
+    }
+    enhanceResult(left)
+    enhanceResult(right)
+    enhanceResult(top)
+    enhanceResult(bottom)
+
+    result
+  }
+
+  val basins = lowPoints.map({ case (Coord(x, y), value) => exploreBasin(Coord(x, y)) }).toSeq
+    .sortBy(_.size).slice(lowPoints.size - 3, lowPoints.size)
+  println(basins)
+  println(basins.map(_.size).mkString(" "))
+  println(s"Part 2: ${basins.map(_.size).reduce((left, right) => left * right)}")
 }
