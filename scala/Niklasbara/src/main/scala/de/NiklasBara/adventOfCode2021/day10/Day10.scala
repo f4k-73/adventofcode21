2e738361b8719949fe75f2e767870d89072b83f0
NiklasBara <n.baranowski@mail.de>
2021-12-11 13:56:51 +0100

    day10 - part1

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day10/Day10.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day10/Day10.scala
@@ -1,67 +1,63 @@
 package de.NiklasBara.adventOfCode2021.day10
 
 import java.nio.charset.StandardCharsets
+import scala.collection.mutable
 import scala.io.Source
 
-object Day9 extends App {
+object Day10 extends App {
   val INPUT_PATH = Inputs.local
   val source = Source.fromFile(INPUT_PATH.toString, StandardCharsets.UTF_8.toString)
   val lines = source.getLines().toSeq
   source.close()
 
-  val value: Seq[Seq[Int]] = lines.map(_.toCharArray.map(_.asDigit).toSeq)
+  val pairs = Map(
+    '(' -> ')', ')' -> '(',
+    '{' -> '}', '}' -> '{',
+    '[' -> ']', ']' -> '[',
+    '<' -> '>', '>' -> '<',
+  )
+
+  val score = Map(
+    ')' -> 3,
+    '}' -> 1197,
+    ']' -> 57,
+    '>' -> 25137,
+  )
+
+  println("part1:", lines.map(part1).groupMapReduce(identity)(identity)(_ + _).values.sum)
 
 
-  val offsets = Seq((-1, 0), (0, -1), (1, 0), (0, 1))
-
-
-  println("part1:", value.lowPoints().map(_._1 + 1).sum)
-  println("part2:", value.basins().map(_.size).sorted.takeRight(3).reduce(Math.multiplyExact))
-
-
-  implicit class Better2dSeq(val s: Seq[Seq[Int]]) {
-    def getCell(y: Int, x: Int): (Int, Int, Int) = {
-      if (x < 0 || y < 0) {
-        (Int.MaxValue, y, x)
-      } else if (y > s.length - 1 || x > s(y).length - 1) {
-        (Int.MaxValue, y, x)
-      } else {
-        (s.get(y, x), y, x)
+  private def part1(line: String) = {
+    val stack = mutable.Stack[Char]()
+    line.foldLeft(0)((number, char) => {
+      if (number == 0) {
+        char match {
+          case ')' | '}' | ']' | '>' =>
+            if (stack.pop() != pairs(char)) {
+              score(char)
+            } else {
+              number
+            }
+          case _ =>
+            stack.push(char)
+            number
+        }
+      }
+      else {
+        number
       }
     }
+    )
+  }
 
-    def basins(): Seq[Seq[(Int, Int, Int)]] = {
-      lowPoints().map(c => grow(c).toSeq)
-    }
+  private def part2(line: String) = {
 
-    def grow(cell: (Int, Int, Int)): Set[(Int, Int, Int)] = {
-      val higherNeighbours = offsets.map(getNeighbour(cell._2, cell._3, _))
-        .filter(_._1 < 9)
-        .filter(_._1 > cell._1)
-      if (higherNeighbours.isEmpty) {
-        Set(cell)
-      } else {
-        higherNeighbours.flatMap(grow).appended(cell).toSet
-      }
-    }
-
-    def getNeighbour(y: Int, x: Int, offset: (Int, Int)): (Int, Int, Int) = getCell(y + offset._1, x + offset._2)
-
-    def isLowPoint(y: Int, x: Int): Boolean = offsets.map(getNeighbour(y, x, _)).map(_._1).forall(_ > getCell(y, x)._1)
-
-    def lowPoints(): Seq[(Int, Int, Int)] = s.zipWithIndex
-      .flatMap(lineAndIndex => lineAndIndex._1.zipWithIndex
-        .filter(cellAndIndex => isLowPoint(lineAndIndex._2, cellAndIndex._2))
-        .map(cellAndIndex => (cellAndIndex._1, lineAndIndex._2, cellAndIndex._2))
-      )
-
-    def get(y: Int, x: Int): Int = s(y)(x)
   }
 }
 
 object Inputs extends Enumeration {
   type Inputs = Value
-  val test: Inputs.Value = Value("src/main/resources/day9/test_input.txt")
-  val local: Inputs.Value = Value("src/main/resources/day9/input.txt")
-  val shared: Inputs.Value = Value("../../data/day9.txt")
+  val test: Inputs.Value = Value("src/main/resources/day10/test_input.txt")
+  val local: Inputs.Value = Value("src/main/resources/day10/input.txt")
+  val shared: Inputs.Value = Value("../../data/day10.txt")
 }
