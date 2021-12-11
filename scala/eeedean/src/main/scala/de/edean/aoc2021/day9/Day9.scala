637a0b2bbf0eb7d2a6119832584f6492581c8adf
Dean Eckert <dean.eckert@red-oak-consulting.com>
2021-12-11 10:59:14 +0100

    I killed a var, and I liked it! ⭐️⭐️

--- scala/eeedean/src/main/scala/de/edean/aoc2021/day9/Day9.scala
+++ scala/eeedean/src/main/scala/de/edean/aoc2021/day9/Day9.scala
@@ -1,5 +1,7 @@
 package de.edean.aoc2021.day9
 
+import apple.laf.JRSUIConstants.Direction
+
 import scala.io.Source
 import scala.language.postfixOps
 
@@ -23,38 +25,57 @@ object Day9 extends App {
     }
   }
 
-  val coords = input.zipWithIndex.map({ case (str, i) => (i, str.zipWithIndex) }).flatMap({ case (y, xs) => xs.map({case (char, x) => (Coord(x, y), char.asDigit)})}).toMap
-  val sortedCoordSeq = coords.toSeq.sorted
+  val coordSystem = input.zipWithIndex.map({ case (str, i) => (i, str.zipWithIndex) }).flatMap({ case (y, xs) => xs.map({case (char, x) => (Coord(x, y), char.asDigit)})}).toMap
+  val sortedCoordSeq = coordSystem.toSeq.sorted
   // println((0 to maxY).map(y => (0 to maxX).map(x => coords(Coord(x, y))).mkString).mkString("\n"))
   def findNeighbourVals(c: Coord): Seq[Int] = {
     val (left, right, top, bottom) = (Coord(c.x - 1, c.y), Coord(c.x + 1, c.y), Coord(c.x, c.y - 1), Coord(c.x, c.y + 1))
     Seq(
-      coords.get(left).getOrElse(10),
-      coords.get(right).getOrElse(10),
-      coords.get(top).getOrElse(10),
-      coords.get(bottom).getOrElse(10)
+      coordSystem.get(left).getOrElse(10),
+      coordSystem.get(right).getOrElse(10),
+      coordSystem.get(top).getOrElse(10),
+      coordSystem.get(bottom).getOrElse(10)
     )
   }
   // if all neighbours are lower / value is higher than/equal to none of them => it's a low point!
-  val lowPoints = coords.filter({ case (coord, value) => findNeighbourVals(coord).filter(_ <= value).isEmpty })
+  val lowPoints = coordSystem.filter({ case (coord, value) => findNeighbourVals(coord).filter(_ <= value).isEmpty })
   println(s"There are ${lowPoints.size} low points: $lowPoints")
   val riskLevelSum = lowPoints.map(_._2 + 1).sum
   println(s"Part 1: $riskLevelSum")
 
-  def exploreBasin(c: Coord, oldResultSet: Set[(Coord, Int)] = Set()): Set[(Coord, Int)] = {
-    val (left, right, top, bottom) = (Coord(c.x - 1, c.y), Coord(c.x + 1, c.y), Coord(c.x, c.y - 1), Coord(c.x, c.y + 1))
-    var result = oldResultSet + coords.get(c).map((c, _)).get
-    def enhanceResult(coord: Coord) = {
-      val opt = coords.get(coord).map((coord, _))
-      if (opt.exists(_._2 < 9) && !result.contains(opt.get))
-        result = result ++ exploreBasin(coord, result)
-    }
-    enhanceResult(left)
-    enhanceResult(right)
-    enhanceResult(top)
-    enhanceResult(bottom)
+  trait Direction {
+    def next: Option[Direction]
+    def coords(coord: Coord): Coord
+  }
+  case object Left extends Direction {
+    override def next = Some(Right)
+    override def coords(c: Coord) = Coord(c.x - 1, c.y)
+  }
+  case object Right extends Direction {
+    override def next = Some(Bottom)
+    override def coords(c: Coord) = Coord(c.x + 1, c.y)
+  }
+  case object Bottom extends Direction {
+    override def next = Some(Top)
+    override def coords(c: Coord) = Coord(c.x, c.y - 1)
+  }
+  case object Top extends Direction {
+    override def next = None
+    override def coords(c: Coord) = Coord(c.x, c.y + 1)
+  }
 
-    result
+  def exploreBasin(currentCoords: Coord, oldResult: Set[(Coord, Int)] = Set(), direction: Option[Direction] = Some(Left)): Set[(Coord, Int)] = {
+    val result = oldResult + coordSystem.get(currentCoords).map((currentCoords, _)).get
+    if(!direction.isEmpty) {
+      val dir = direction.get
+      val nextCoords = dir.coords(currentCoords)
+      val opt = coordSystem.get(nextCoords).map((nextCoords, _))
+      if (opt.exists(_._2 < 9) && !result.contains(opt.get))
+        exploreBasin(nextCoords, result ++ exploreBasin(currentCoords, result, dir.next))
+      else
+        exploreBasin(currentCoords, result, dir.next)
+    } else
+      result
   }
 
   val basins = lowPoints.map({ case (Coord(x, y), value) => exploreBasin(Coord(x, y)) }).toSeq
