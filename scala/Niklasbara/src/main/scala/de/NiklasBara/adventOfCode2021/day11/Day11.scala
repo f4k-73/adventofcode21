3660aa24a1ce240c65fecc1dd64f515baab23175
NiklasBara <n.baranowski@mail.de>
2021-12-17 15:42:44 +0100

    dirty day 11 - part1

--- scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day11/Day11.scala
+++ scala/Niklasbara/src/main/scala/de/NiklasBara/adventOfCode2021/day11/Day11.scala
@@ -1,118 +1,99 @@
 package de.NiklasBara.adventOfCode2021.day11
 
-import java.nio.charset.StandardCharsets
-import java.util.function.Predicate
-import scala.io.Source
+import de.NiklasBara.adventOfCode2021.input.{AdventDays, InputReader, InputTypes}
+
 
 object Day11 extends App {
-  val INPUT_PATH = Inputs.test
-  val source = Source.fromFile(INPUT_PATH.toString, StandardCharsets.UTF_8.toString)
-  val lines = source.getLines().map(_.toCharArray.map(_.asDigit).toSeq).toSeq
-  source.close()
+  val inputs = InputReader(AdventDays.ELEVEN, InputTypes.local).lines.map(_.toCharArray.map(_.asDigit).toSeq)
+
+  var flashes = 0
 
   val offsets = Seq((-1, 0), (0, -1), (1, 0), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1))
+  var flashCounter = 0;
 
-
-  var res = lines
-  for(x <- 0 until 100){
-   res = res.incrementAll(1).resolveFlashes.zeroFlashed
-  }
-  println(res.amountFlashes)
+  var res = inputs.simulateSteps(100)
+  println("part1: ", flashes)
 
   implicit class Better2dInts(s: Seq[Seq[Int]]) {
 
-    var amountFlashes = 0
-
-
-    private def getOrElse(y: Int, x: Int, elseValue: Int): (Int, Int, Int) = {
+    def get(y: Int, x: Int): Int = {
       if (x < 0 || y < 0) {
-        (elseValue, y, x)
+        0
       } else if (y > s.length - 1 || x > s(y).length - 1) {
-        (elseValue, y, x)
+        0
       } else {
-        (s(y)(x), y, x)
+        s(y)(x)
       }
     }
 
-    def get(y: Int, x: Int): (Int, Int, Int) = {
-      getOrElse(y, x, Int.MaxValue)
+    def simulateStep: Seq[Seq[Int]] = {
+      s.incrementAll(1).handleFlashes
     }
 
-    def findAll(predicate: Predicate[Int]): Seq[(Int, Int, Int)] = {
-      var res = Seq[(Int, Int, Int)]()
-      for (y <- s.indices) {
-        for (x <- s(y).indices) {
-          if (predicate.test(s.get(y, x)._1)) {
-            res = res.appended(s.get(y, x))
-          }
-        }
-      }
-      res
-    }
-
-    def getNeighbour(y: Int, x: Int, offset: (Int, Int)): (Int, Int, Int) = get(y + offset._1, x + offset._2)
-
-    def incrementAll(n: Int): Seq[Seq[Int]] = {
-      s.map(row => row.map(cell => cell + n))
-    }
-
-    def incrementAllContainedIn(n: Int, container: Set[(Int, Int, Int)]): Seq[Seq[Int]] = {
-      s.zipWithIndex.map(rowAndIndex => {
-        val y = rowAndIndex._2
-        rowAndIndex._1.zipWithIndex.map(colAndIndex => {
-          val x = colAndIndex._2
-          val cell = (s(y)(x), y, x)
-          if (container.contains(cell)) {
-            cell._1 + n
-          } else {
-            cell._1
-          }
-        })
-      })
-
-    }
-
-    def resolveFlashes: Seq[Seq[Int]] = {
-      val flashing = s.findAll(_.equals(9))
-      if (flashing.isEmpty) {
+    def simulateSteps(amount: Int): Seq[Seq[Int]] = {
+      if (amount <= 0) {
         s
       } else {
-        val neighboursToIncrement: Set[(Int, Int, Int)] = offsets.flatMap(offset => {
-          flashing.map(cell => {
-            s.getNeighbour(cell._2, cell._3, offset)
-          })
-        }).filter(_._1 < 9).toSet
-
-        s.incrementAllContainedIn(1, neighboursToIncrement).incrementAllContainedIn(1, flashing.toSet).resolveFlashes
+        simulateStep.simulateSteps(amount - 1)
       }
     }
 
-    def zeroFlashed: Seq[Seq[Int]] = {
-      s.setAll {
-        case 10 | 9 => 0
-        case value => value
+    def handleFlashes: Seq[Seq[Int]] = {
+      var flashOccurred = false
+      var incremented: Seq[(Int, Int)] = Seq()
+      val flashedArray = s.zipWithIndex.map(rowAndY => {
+        rowAndY._1.zipWithIndex.map(cellAndX => {
+          if (s.get(rowAndY._2, cellAndX._2) > 9) {
+            flashes += 1
+            flashOccurred = true
+            incremented = incremented.appended(rowAndY._2, cellAndX._2)
+            0
+          } else {
+            cellAndX._1
+          }
+        })
+      }).incrementAllContained(1, getNeighbors(incremented))
+
+      if (flashOccurred) {
+        flashedArray.handleFlashes
+      } else {
+        flashedArray
       }
+    }
+
+    def getNeighbors(points: Seq[(Int, Int)]): Seq[(Int, Int)] = {
+      points.flatMap(point => offsets.map(off => (point.y + off.y, point.x + off.x)))
+    }
+
+    def incrementAllContained(n: Int, contained: Seq[(Int, Int)]): Seq[Seq[Int]] = {
+      s.zipWithIndex.map(rowAndIndex =>
+        rowAndIndex._1.zipWithIndex.map(cellAndIndex => {
+          incrementIfValid(n, contained, rowAndIndex._2, cellAndIndex._2, cellAndIndex._1)
+        })
+      )
+    }
+
+    private def incrementIfValid(n: Int, contained: Seq[(Int, Int)], y: Int, x: Int, value: Int): Int = {
+      if (value != 0) {
+        value + (n * contained.count(point => point == (y, x)))
+      } else {
+        0
+      }
+    }
+
+    def incrementAll(n: Int): Seq[Seq[Int]] = {
+      setAll(_ + n)
     }
 
     def setAll(function: Function[Int, Int]): Seq[Seq[Int]] = {
       s.map(row => row.map(function.apply))
     }
 
-    def simulate(steps: Int) = {
-      if (steps > 0) {
-        s.incrementAll(1).resolveFlashes.zeroFlashed
-      } else {
-        s
-      }
-    }
-
   }
-}
 
+  implicit class Point(p: (Int, Int)) {
+    def y: Int = p._1
 
-object Inputs extends Enumeration {
-  type Inputs = Value
-  val test: Inputs.Value = Value("src/main/resources/day11/test_input.txt")
-  val local: Inputs.Value = Value("src/main/resources/day11/input.txt")
-  val shared: Inputs.Value = Value("../../data/day11.txt")
+    def x: Int = p._2
+  }
 }
